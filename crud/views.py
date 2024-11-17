11e859fc96a2d425baeef3784d9ec8026584818e
Danial Sakhpantayev <danial.sakhpantayev@gmail.com>
2024-11-17 16:24:07 +0500

    fix of record creation

--- crud/views.py
+++ crud/views.py
@@ -143,83 +143,72 @@ def view_records(request, check):
 @api_view(["POST"])
 @permission_classes([AllowAny])
 def create_record(request):
-    if request.method == "POST":
-        email = request.data.get("email")
-        cname = request.data.get("cname")
-        disease_code = request.data.get("disease_code")
-        total_deaths = request.data.get("total_deaths")
-        total_patients = request.data.get("total_patients")
+    email = request.data.get("email")
+    cname = request.data.get("cname")
+    disease_code = request.data.get("disease_code")
+    total_deaths = request.data.get("total_deaths")
+    total_patients = request.data.get("total_patients")
 
-        # Check if the PublicServant exists
-        public_servant = Publicservant.objects.filter(email=email).first()
-        if not public_servant:
-            return Response(
-                {"error": "The provided email does not belong to a PublicServant."},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
+    try:
+        public_servant = Publicservant.objects.get(email=email)
+    except Exception:
+        return Response(
+            {"error": "public servant doesn't exist"},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
 
-        # Check if the combination of email, cname, and disease_code already exists
-        if Record.objects.filter(
-            email=public_servant, cname=cname, disease_code=disease_code
-        ).exists():
-            return Response(
-                {
-                    "error": "Record with the same email, country, and disease code already exists."
-                },
-                status=status.HTTP_400_BAD_REQUEST,
-            )
+    if Record.objects.filter(
+        email=public_servant, cname=cname, disease_code=disease_code
+    ).exists():
+        return Response(
+            {
+                "error": "Record with the same email, country, and disease code already exists."
+            },
+            status=status.HTTP_400_BAD_REQUEST,
+        )
 
-        # Check if the country exists
-        country = Country.objects.filter(cname=cname).first()
-        if not country:
-            url = "https://countriesnow.space/api/v0.1/countries/population"
-            data = {"country": cname}
-            response = requests.post(url, data=data)
-            if response.status_code == 200:
-                response_data = response.json()
-                try:
-                    population = response_data.get("data")["populationCounts"][-1][
-                        "value"
-                    ]
-                    country = Country.objects.create(cname=cname, population=population)
-                except (KeyError, IndexError):
-                    return Response(
-                        {
-                            "error": "Population data for the country could not be retrieved."
-                        },
-                        status=status.HTTP_400_BAD_REQUEST,
-                    )
-            else:
+    try:
+        country = Country.objects.get(cname=cname)
+    except Exception:
+        url = "https://countriesnow.space/api/v0.1/countries/population"
+        response = requests.post(url, json={"country": cname})
+        if response.status_code == 200:
+            try:
+                population = response.json()["data"]["populationCounts"][-1]["value"]
+                country = Country.objects.create(cname=cname, population=population)
+            except Exception:
                 return Response(
-                    {"error": "Request failed."},
+                    {"error": "Error during fetching"},
                     status=status.HTTP_400_BAD_REQUEST,
                 )
-        disease = Disease.objects.filter(disease_code=disease_code).first()
-        if not disease:
+        else:
             return Response(
-                {"error": "The provided disease code does not exist."},
+                {"error": "Country info wasn't fetched."},
                 status=status.HTTP_400_BAD_REQUEST,
             )
 
-        if not total_deaths or not total_patients:
-            return Response(
-                {"error": "Total deaths and total patients are required."},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-        record_data = {
-            "email": public_servant,
-            "cname": country,
-            "disease_code": disease,
-            "total_deaths": total_deaths,
-            "total_patients": total_patients,
-        }
-        try:
-            record = Record.objects.create(**record_data)
-            return Response(
-                RecordSerializer(record).data, status=status.HTTP_201_CREATED
-            )
-        except Exception as e:
-            return Response(
-                {"error": f"Error creating record: {str(e)}"},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
+    try:
+        disease = Disease.objects.get(disease_code=disease_code)
+    except Exception:
+        return Response(
+            {"error": "The provided disease code does not exist."},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+
+    if not total_deaths or not total_patients:
+        return Response(
+            {"error": "Total deaths and total patients are required."},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+
+    try:
+        record = Record.objects.create(
+            email=public_servant,
+            cname=country,
+            disease_code=disease,
+            total_deaths=total_deaths,
+            total_patients=total_patients,
+        )
+        return Response(RecordSerializer(record).data)
+    except Exception:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
